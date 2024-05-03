ce0a96f2a7d782e0d2012701112d5f8ce838ec4d
Jared Patterson <jpwesley34@gmail.com>
2024-05-03 11:18:21 -0700

    Passes tests.

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        anagram = []
+        for words in list:
+            if sorted([letter for letter in words]) == sorted([letter for letter in self.word]):
+                anagram.append(words)
+            else: 
+                return anagram
